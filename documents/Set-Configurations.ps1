fdc2551a5f6bade9f6025143735a09fb51986199
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-05-05 18:46:31 -0500

    Add files via upload

---
+++ documents/Set-Configurations.ps1
@@ -0,0 +1,21 @@
+﻿# What do I want to do?
+
+# How will I Implement it?
+
+
+#netsh http add urlacl url=http://+:80MyUri user=DOMAIN\user
+$VerbosePreference = "Continue";
+$ErrorActionPreference = "Stop";
+
+$Domain = [Environment]::UserDomainName;
+$User = [Environment]::UserName
+
+$Command = "netsh http add urlacl url=http://+:8080/ user=$Domain\$User";
+Write-Verbose -Message $Command;
+Invoke-Expression -Command $Command;
+
+$Command = "netsh http add urlacl url=http://+:8090/ user=$Domain\$User";
+Write-Verbose -Message $Command;
+Invoke-Expression -Command $Command;
+
+control
