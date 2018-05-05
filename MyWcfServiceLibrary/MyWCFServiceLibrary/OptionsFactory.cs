14af926d39f517a946de295d33bdcd3c6844acc3
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-05-05 18:49:18 -0500

    Update OptionsFactory.cs

--- MyWcfServiceLibrary/MyWCFServiceLibrary/OptionsFactory.cs
+++ MyWcfServiceLibrary/MyWCFServiceLibrary/OptionsFactory.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace MyWCFServiceLibrary
 {
@@ -7,17 +7,10 @@ namespace MyWCFServiceLibrary
         public string OptionA;
         public string OptionB;
 
-        //public List<Option> listOptions = new List<Option>();
-        //MyWCFServiceLibrary.Option myOption = new MyWCFServiceLibrary.Option();
-        //MyWCFServiceLibrary.Option option = new Option();
-        //MyWCFServiceLibrary.OptionsFactory optionsFactory = new OptionsFactory(option);
-
         public OptionsFactory()//Option option)
         {
             this.OptionA = "string GetMessage(string name);";
             this.OptionB = "GetSpawnFromProcessFactory(string computername);";
-            //option.SetOptionA(thioptionA);
-            //option.SetOptionB(optionB);
         }
     }
 }
